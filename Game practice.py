364a374d11c14c44a40409369e61da43b4728cdf
BWilliams17 <88779475+BWilliams17@users.noreply.github.com>
2022-03-07 12:15:16 +0000

    Update Game practice.py

--- Game practice.py
+++ Game practice.py
@@ -1,31 +1,40 @@
 def main():
     print("Welcome to the game")
-    player_name=input("What is your name? ")
-    print("Hi "+player_name)
+    player_name=input("What is your name?\n\n")
+    print("\nHi "+player_name)
     lives=3
     print(f"You have {lives} lives remaining")
-    print("There are four doors presented to you, Forward, Right, Left, Backwards")
-    direction=input("Which direction do you want to go? ")
-    while direction != "Forward"+"Left"+"Right"+"Backwards":
-     if direction=="Forward":
-       print("You walk forwards towards the door infront of you")
-       break
+    print("\nYou wake up in the middle of a room with no recollection of anything before that.")
+    print("There are four doors presented to you, Forward, Right, Left, Backwards.")
+    direction=input("Which direction do you want to go?\n\nI walked ")
+    if direction != "Forward"+"Left"+"Right"+"Backwards":
+     if direction=="Forward" or "Fire":
+       print("\nYou walk forwards towards the door infront of you")
+       print("You walk into a room and filled with the immense hear of fire embers floating aroud the room.")
+       print("A message reveals itself over a roaring flame.")
+       print("It reads: There is a man that can never enjoy the fireplace, Who is this man?\n")
+       print("You can either type; Answer or Hub.\n")
+       action=input("What do you do?\n\n")
+       if action=="Answer":
+         answer=input("\nWhat is your answer?\n\n")
+         if answer="snowman":
+           print("The flame distinguishes and a light glows in the middle room")
+         else:
+           print("")
+
      elif direction=="Right":
        print("You walk through the door on your right")
-       break
      elif direction=="Left":
        print("You walk towards the door on your left")
-       break
      elif direction=="Backwards":
        print("You turn around and walk through the door")
-       break
      else:
-       print("There is no door in that direction and you walked into a solid wall")
+       print("There is no door in that direction and you have walked into a solid wall")
        print("You lose a life")
        lives -=1
        if lives<=0:
         print("You have just died")
-        break
+        end
        print(f"You have {lives} lives remaining")
        direction=input("Which direction do you want to go? ")
 main()
